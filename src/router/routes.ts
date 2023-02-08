bdf86a30216359a55c86ddf16b9f01be75541d0d
dongnt <nguyenthanhdong0710@gmail.com>
2023-02-09 03:29:13 +0700

    small change

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,5 +1,10 @@
 import HomeView from "../views/HomeView.vue";
 
+// route level code-splitting
+// this generates a separate chunk (About.[hash].js) for this route
+// which is lazy-loaded when the route is visited.
+const AboutView = () => import("@/views/AboutView.vue");
+
 export default [
   {
     path: "/",
@@ -9,9 +14,6 @@ export default [
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import("../views/AboutView.vue"),
+    component: AboutView,
   },
 ];
